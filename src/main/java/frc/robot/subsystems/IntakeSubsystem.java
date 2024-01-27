5f76cbac78ba79b587d01e779af443f44901e431
LCMadsen <bluejacketprogramming@gmail.com>
2024-01-27 11:52:29 -0600

    hardware declare

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -1,5 +1,9 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
 public class IntakeSubsystem {
-    
+    CANSparkMax takeLeft = new CANSparkMax(0, MotorType.kBrushless);
+    CANSparkMax takeRight = new CANSparkMax(1, MotorType.kBrushless);
 }
