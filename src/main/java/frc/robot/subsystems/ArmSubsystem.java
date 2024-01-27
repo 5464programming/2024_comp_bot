1bccbced1db93bca4bd64c1c5009d3032ead2b66
Team5464 <94130236+5464programming@users.noreply.github.com>
2024-01-27 12:10:01 -0600

    hardware decler
    eeeeeeeeeeeeeeeeeeee

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -1,5 +1,9 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
 public class ArmSubsystem {
-    
+    CANSparkMax leftarm = new CANSparkMax(6, MotorType.kBrushless);
+    CANSparkMax rightarm = new CANSparkMax(17, MotorType.kBrushless);   
 }
