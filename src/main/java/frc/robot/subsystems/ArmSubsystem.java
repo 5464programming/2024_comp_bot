c0b0e7183ccddf742475bcd99869eace3f7aa390
Team5464 <94130236+5464programming@users.noreply.github.com>
2024-01-27 13:53:28 -0600

    hardware decler
    yep

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -3,7 +3,13 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.DigitalInput;
+
 public class ArmSubsystem {
     CANSparkMax leftarm = new CANSparkMax(6, MotorType.kBrushless);
-    CANSparkMax rightarm = new CANSparkMax(17, MotorType.kBrushless);   
+    CANSparkMax rightarm = new CANSparkMax(17, MotorType.kBrushless);  
+    DigitalInput limitLup = new DigitalInput(1);
+    DigitalInput limitRup = new DigitalInput(2);
+    DigitalInput limitLdown = new DigitalInput(3);
+    DigitalInput limitRdown = new DigitalInput(4);
 }
