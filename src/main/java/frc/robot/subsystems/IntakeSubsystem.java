48c9737665fad9d7a28df5e0352ad2d8fb74ee9d
Team5464 <94130236+5464programming@users.noreply.github.com>
2024-02-03 11:17:56 -0600

    command

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -2,15 +2,23 @@ package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
-
+import entech.subsystems.EntechSubsystem;
 import edu.wpi.first.wpilibj.DigitalInput;
 import frc.robot.OI.UserPolicy;
 
-public class IntakeSubsystem {
+public class IntakeSubsystem extends EntechSubsystem{
     CANSparkMax intake = new CANSparkMax(9, MotorType.kBrushless);
     DigitalInput searchnote = new DigitalInput(0);
-
+    private static final boolean ENABLED = true;
     private boolean notedected = false;
+
+    @Override
+    public void initialize(){}
+
+    @Override
+    public boolean isEnabled() {
+        return ENABLED;
+    }
 
     public void DisableIntake(){
         intake.set(0);
