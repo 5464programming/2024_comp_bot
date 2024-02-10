5238fa072dc71a57aab506a2a05325ee2c3f0f60
Team5464 <94130236+5464programming@users.noreply.github.com>
2024-02-10 13:52:09 -0600

    auto update

--- src/main/java/frc/robot/CommandFactory.java
+++ src/main/java/frc/robot/CommandFactory.java
@@ -24,4 +24,31 @@ public class CommandFactory {
         
         return auto;
     }
+    
+    public Command BluePos2(){
+        SequentialCommandGroup auto = new SequentialCommandGroup();
+
+        auto.addCommands(new GyroReset(driveSubsystem));
+        auto.addCommands(new PathPlannerAuto("BluePos2"));
+        
+        return auto;
+    }
+
+    public Command BluePos3(){
+        SequentialCommandGroup auto = new SequentialCommandGroup();
+
+        auto.addCommands(new GyroReset(driveSubsystem));
+        auto.addCommands(new PathPlannerAuto("BluePos3"));
+        
+        return auto;
+    }
+
+    public Command BluePos4(){
+        SequentialCommandGroup auto = new SequentialCommandGroup();
+
+        auto.addCommands(new GyroReset(driveSubsystem));
+        auto.addCommands(new PathPlannerAuto("BluePos4"));
+        
+        return auto;
+    }
 }
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -6,6 +6,7 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.OI.OperatorInterface;
@@ -46,6 +47,8 @@ public class Robot extends TimedRobot {
         subsystemManager.getIntakeSubsystem().periodic();  
         subsystemManager.getVisionSubsystem().periodic(); 
         subsystemManager.getShooterSubsystem().periodic();
+
+        SmartDashboard.putData("Auto choices", auto_chooser);
     }
 
     @Override
