fa0b6fd42f3f93fd8f7ad664a76b8567983a4603
LCMadsen <bluejacketprogramming@gmail.com>
2024-02-12 18:29:59 -0600

    IVE BEEN WORKING......ON THE RAILROAD
    YAY

--- src/main/java/frc/robot/CommandFactory.java
+++ src/main/java/frc/robot/CommandFactory.java
@@ -3,10 +3,13 @@ package frc.robot;
 import com.pathplanner.lib.commands.PathPlannerAuto;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.commands.GyroReset;
+import frc.robot.commands.IntakeCommand;
 import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.IntakeSubsystem;
 
 @SuppressWarnings("unused")
 public class CommandFactory {
@@ -30,7 +33,6 @@ public class CommandFactory {
 
         auto.addCommands(new GyroReset(driveSubsystem));
         auto.addCommands(new PathPlannerAuto("BluePos2"));
-        
         return auto;
     }
 
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -12,7 +12,12 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.OI.OperatorInterface;
+import frc.robot.commands.AmpShootCommand;
+import frc.robot.commands.BackflipCommand;
+import frc.robot.commands.FeedCommand;
+import frc.robot.commands.GyroReset;
 import frc.robot.commands.IntakeCommand;
+import frc.robot.commands.SpeakerShootCommand;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -28,6 +33,10 @@ public class Robot extends TimedRobot {
     private SubsystemManager subsystemManager;
     private CommandFactory commandFactory;
     private IntakeCommand intakeCommand;
+    private AmpShootCommand ampShootCommand;
+    private SpeakerShootCommand speakerShootCommand;
+    private FeedCommand feedCommand;
+    private GyroReset gyroReset;
 
     private String auto_selected;
     private final SendableChooser<String> auto_chooser = new SendableChooser<>();
@@ -41,8 +50,16 @@ public class Robot extends TimedRobot {
     public void robotInit() {
         subsystemManager = new SubsystemManager();
         intakeCommand = new IntakeCommand(subsystemManager.getIntakeSubsystem());
+        ampShootCommand = new AmpShootCommand(subsystemManager.getShooterSubsystem());
+        speakerShootCommand = new SpeakerShootCommand(subsystemManager.getShooterSubsystem());
+        feedCommand = new FeedCommand(subsystemManager.getIntakeSubsystem());
+        gyroReset = new GyroReset(subsystemManager.getDriveSubsystem());
         // TODO: Get this initialized correctly, so that it can run in Path Planner
         NamedCommands.registerCommand("IntakeCommand", intakeCommand);
+        NamedCommands.registerCommand("AmpShootCommand", ampShootCommand);
+        NamedCommands.registerCommand("SpeakerShootCommand", speakerShootCommand);
+        NamedCommands.registerCommand("FeedCommand", feedCommand);
+        NamedCommands.registerCommand("GyroReset", gyroReset);
 
         commandFactory = new CommandFactory(subsystemManager);
         OperatorInterface.create(commandFactory, subsystemManager);
